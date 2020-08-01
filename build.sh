8e50d464ed0c1a254f71aaf20670aa11753155aa
brewmaster93 <corristo.tmw@gmail.com>
2020-08-01 12:17:30 +0200

    first version, added support for docker and build script

--- build.sh
+++ build.sh
@@ -1,1 +1,7 @@
-cd teamcity-minimal-agent && sudo docker build . -t teamcity-minimal-agent:latest && cd ../teamcity-agent && sudo docker build . -t teamcity-agent:latest && cd ../teamcity-server && sudo docker build . -t teamcity-server:latest
+cd context
+wget -c https://download.jetbrains.com/teamcity/TeamCity-2020.1.tar.gz
+tar zxfv TeamCity-*.tar.gz
+sudo docker build . -f ../teamcity-server/Dockerfile -t teamcity-server:latest
+sudo docker build . -f ../teamcity-minimal-agent/Dockerfile -t teamcity-minimal-agent:latest
+sudo docker build . -f ../teamcity-agent/Dockerfile -t teamcity-agent:latest
+sudo docker build . -f ../teamcity-agent-docker/Dockerfile -t teamcity-agent-docker:latest
